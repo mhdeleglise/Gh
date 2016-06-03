6442a7e0c7eef6dc26ca0122ad4c4db326664332
mhdeleglise <m.h.deleglise@gmail.com>
2016-06-03 22:08:56 +0200

    Ajout de prev_prime.cc

---
+++ src/prev_prime.cc
@@ -0,0 +1,11 @@
+#include<gmp.h>
+
+void prevprime(mpz_t rop, mpz_t x) {
+  if (mpz_divisible_2exp_p (x, 1))
+    mpz_sub_ui(x, x, 1);
+
+  mpz_set(rop,x);
+  while (! mpz_probab_prime_p (rop, 100)) {
+    mpz_sub_ui(rop, rop, 2);
+  }
+}
