75268b4ed3d69f1642cc4973fb8e3af4abc2b954
mhdeleglise <m.h.deleglise@gmail.com>
2016-06-04 16:58:48 +0200

    Renvoie le premier precedant strictement q

--- src/prev_prime.cc
+++ src/prev_prime.cc
@@ -3,7 +3,8 @@
 void prevprime(mpz_t rop, mpz_t x) {
   if (mpz_divisible_2exp_p (x, 1))
     mpz_sub_ui(x, x, 1);
-
+  else
+    mpz_sub_ui(x, x, 2);
   mpz_set(rop,x);
   while (! mpz_probab_prime_p (rop, 100)) {
     mpz_sub_ui(rop, rop, 2);
